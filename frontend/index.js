270b417e5ee8698a2e56fe20a25a55f435389dea
dduck00 <ejr075@gmail.com>
2019-05-09 23:18:09 +0900

    post 기능 완성

--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,39 @@
+
 /* eslint-disable linebreak-style */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable max-len */
 const Express = require('express');
 const Web3 = require('web3');
 const app = new Express();
 const web3 = new Web3();
+const bodyParser = require('body-parser');
 const portAddress = 3000;
+web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // TODO:get account balance
 app.get('/', (req, res) => {
-  res.json({message: 'get'});
+  res.json({message: web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]))});
 });
 
-// TODO:send transaction
+
 app.put('/', (req, res) => {
   res.json({message: 'put'});
 });
 
+// TODO:send transaction
 app.post('/', (req, res) => {
+  if (web3.personal.unlockAccount(req.body.from, req.body.pass)) {
+    web3.eth.sendTransaction({from: req.body.from, to: req.body.to, value: req.body.amount}, (err, result)=>{
+      if (!err) {
+        console.log('Transaction is sent Successful!('+result+')');
+      } else {
+        console.log(err);
+      }
+    });
+  }
   res.json({message: 'post'});
 });
 
